ea92b17c0618a7b92650862784d5be08219b2526
tony-waters <2tonywaters@gmail.com>
2026-01-05 17:29:18 +0000

    demo it works

---
+++ src/main/java/uk/bit1/spring_jpa/Customer.java
@@ -0,0 +1,42 @@
+package uk.bit1.spring_jpa;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String lastName;
+    private String firstName;
+
+    protected Customer() {}
+
+    public Customer(String lastName, String firstName) {
+        this.lastName= lastName;
+        this.firstName = firstName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[id=%d, firstName='%s', lastName='%s']",
+                id, firstName, lastName);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+}
